9749575ab3e05d74fc7e787317fbc72410612f68
dizmerly <dan.izmerly@gmail.com>
2025-09-22 18:21:00 -0500

    approximating

---
+++ test/tyr_test_temp.py
@@ -0,0 +1,72 @@
+sex:M age:20 cho:1 smo:N hdl:55 sbp:1 med:Y out:<1 
+sex:M age:35 cho:1 smo:N hdl:55 sbp:1 med:Y out:<1
+sex:M age:40 cho:1 smo:N hdl:55 sbp:1 med:Y out:1 
+sex:M age:45 cho:1 smo:N hdl:55 sbp:1 med:Y out:1 
+sex:M age:50 cho:1 smo:N hdl:55 sbp:1 med:Y out:2 
+sex:M age:55 cho:1 smo:N hdl:55 sbp:1 med:Y out:4 
+sex:M age:60 cho:1 smo:N hdl:55 sbp:1 med:Y out:6 
+sex:M age:65 cho:1 smo:N hdl:55 sbp:1 med:Y out:8 
+sex:M age:70 cho:1 smo:N hdl:55 sbp:1 med:Y out:10 
+sex:M age:75 cho:1 smo:N hdl:55 sbp:1 med:Y out:12 
+##############################
+
+
+sex:F age:55 cho:1 smo:N hdl:55 sbp:1 med:Y out:<1 
+sex:F age:60 cho:1 smo:N hdl:55 sbp:1 med:Y out:1 
+sex:F age:65 cho:1 smo:N hdl:55 sbp:1 med:Y out:1 
+sex:F age:70 cho:1 smo:N hdl:55 sbp:1 med:Y out:2 
+sex:F age:75 cho:1 smo:N hdl:55 sbp:1 med:Y out:4
+
+
+
+
+
+sex:F age:55 cho:1 smo:N hdl:55 sbp:1 med:Y out:<1 
+sex:F age:60 cho:1 smo:N hdl:55 sbp:1 med:Y out:1 
+sex:F age:65 cho:1 smo:N hdl:55 sbp:1 med:Y out:1 
+sex:F age:70 cho:1 smo:N hdl:55 sbp:1 med:Y out:2 
+sex:F age:75 cho:1 smo:N hdl:55 sbp:1 med:Y out:4
+
+#smokers (isolated) Female/M done output
+sex:F age:20 cho:1 smo:Y hdl:55 sbp:1 med:Y out:<1 
+sex:F age:40 cho:1 smo:Y hdl:55 sbp:1 med:Y out:<1 
+sex:F age:50 cho:1 smo:Y hdl:55 sbp:1 med:Y out:1 
+sex:F age:60 cho:1 smo:Y hdl:55 sbp:1 med:Y out:1  
+sex:F age:75 cho:1 smo:Y hdl:55 sbp:1 med:Y out:5 
+#Done Output
+sex:M age:20 cho:1 smo:Y hdl:55 sbp:1 med:Y out:<1
+sex:M age:40 cho:1 smo:Y hdl:55 sbp:1 med:Y out:2 
+sex:M age:50 cho:1 smo:Y hdl:55 sbp:1 med:Y out:5 
+sex:M age:60 cho:1 smo:Y hdl:55 sbp:1 med:Y out:8 
+sex:M age:75 cho:1 smo:Y hdl:55 sbp:1 med:Y out:16  
+
+#Total cholesteral cases, only over 160. 
+sex:M age:20 cho:150 smo:N hdl:55 sbp:1 med:Y out:<1  
+sex:M age:20 cho:161 smo:N hdl:55 sbp:1 med:Y out:<1  
+sex:M age:20 cho:201 smo:N hdl:55 sbp:1 med:Y out:<1  
+sex:M age:20 cho:241 smo:N hdl:55 sbp:1 med:Y out:1  
+sex:M age:20 cho:281 smo:N hdl:55 sbp:1 med:Y out:1 
+sex:M age:35 cho:160 smo:N hdl:55 sbp:1 med:Y out:1
+
+
+sex:M age:40 cho:150 smo:N hdl:55 sbp:1 med:Y out:1   
+sex:M age:40 cho:161 smo:N hdl:55 sbp:1 med:Y out:1  
+sex:M age:40 cho:201 smo:N hdl:55 sbp:1 med:Y out:2   
+sex:M age:40 cho:241 smo:N hdl:55 sbp:1 med:Y out:2   
+sex:M age:40 cho:281 smo:N hdl:55 sbp:1 med:Y out:4   
+
+sex:M age:45 cho:199 smo:N hdl:55 sbp:1 med:Y out:2   
+
+sex:M age:50 cho:150 smo:N hdl:55 sbp:1 med:Y out:2   
+sex:M age:50 cho:161 smo:N hdl:55 sbp:1 med:Y out:4   
+sex:M age:50 cho:201 smo:N hdl:55 sbp:1 med:Y out:5   
+sex:M age:50 cho:241 smo:N hdl:55 sbp:1 med:Y out:6   
+sex:M age:50 cho:281 smo:N hdl:55 sbp:1 med:Y out:8   
+
+sex:M age:55 cho:220 smo:N hdl:55 sbp:1 med:Y out:10  
+
+sex:M age:60 cho:150 smo:N hdl:55 sbp:1 med:Y out:6   
+sex:M age:60 cho:161 smo:N hdl:55 sbp:1 med:Y out:8   
+sex:M age:60 cho:201 smo:N hdl:55 sbp:1 med:Y out:8   
+sex:M age:60 cho:241 smo:N hdl:55 sbp:1 med:Y out:10   
+sex:M age:60 cho:281 smo:N hdl:55 sbp:1 med:Y out:12  
